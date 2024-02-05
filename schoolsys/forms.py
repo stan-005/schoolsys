65019897dbc26d1fcbafdb43228e1b8d245537d8
Nehemiah60 <nehemiabosire41@gmail.com>
2024-02-05 22:06:27 +0300

    performing crud on students

--- schoolsys/forms.py
+++ schoolsys/forms.py
@@ -19,3 +19,14 @@ class StudentUpdateForm(FlaskForm):
     fee_paid      = DecimalField('Fee Paid', default=0)
     fee_total     = DecimalField('Fee Total', validators=[DataRequired()])
     submit        = SubmitField()
+
+class TeacherForm(FlaskForm):
+    first_name    = StringField('First Name',  validators=[DataRequired(), Length(min=2, max=20)])
+    second_name   = StringField('Second Name', validators=[DataRequired(), Length(min=2, max=20)])
+    submit        = SubmitField('Hire')
+
+class TeacherUpdateForm(FlaskForm):
+    first_name    = StringField('First Name',  validators=[DataRequired(), Length(min=2, max=20)])
+    second_name   = StringField('Second Name', validators=[DataRequired(), Length(min=2, max=20)])
+    submit        = SubmitField('Update')
+    
