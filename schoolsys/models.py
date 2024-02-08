2208e4464efa35b8aec386bc5f4a7bb666d25bf6
Nehemiah60 <nehemiabosire41@gmail.com>
2024-02-08 17:54:13 +0300

    added grades

--- schoolsys/models.py
+++ schoolsys/models.py
@@ -8,6 +8,7 @@ class Students(db.Model):
     second_name = db.Column(db.String(100), unique=False, index=True)
     fee_paid    = db.Column(db.Numeric(10, 2), nullable=False, default=0)
     fee_total   = db.Column(db.Numeric(10,2), nullable=False)
+    grade_id    = db.Column(db.Integer, db.ForeignKey('grade.id'), nullable=False)
 
     def __repr__(self):
         return f" Students('{self.first_name}', '{self.second_name}', '{self.email}') "
@@ -26,3 +27,14 @@ class Teachers(db.Model):
     first_name    = db.Column(db.String(100), unique=False, index=True)
     second_name   = db.Column(db.String(100), unique=False, index=True)
     joined_date   = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Grades(db.Model):
+    __tablename__ = 'grade'
+    id            = db.Column(db.Integer, primary_key=True)
+    name          = db.Column(db.String(100))
+    levels        = db.Column(db.String(100))
+    students      = db.relationship('Students', backref='grade', lazy=True)
+
+    def count_students(self):
+        return len(self.students)
+    
